44f9e3860d6232b42c43a379019b0ffe2d56a1b0
jayster17cabay-cell <jayster17cabay@gmail.com>
2026-09-04 11:33:50 +0800

    Remove dead Mailjet transport code after switching to Brevo (Sendinblue) mailer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use App\Http\ViewComposers\NotificationComposer;
-use App\Mail\MailjetTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,9 +24,5 @@ class AppServiceProvider extends ServiceProvider
         }
 
         View::composer(['layouts.tfrb-officer', 'layouts.superadmin', 'notifications.index'], NotificationComposer::class);
-
-        Mail::extend('mailjet', function (array $config) {
-            return new MailjetTransport($config['api_key'] ?? '', $config['secret_key'] ?? '');
-        });
     }
 }
